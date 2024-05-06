a9bc285603a702c6141e2b7b0b92f6d0e32fe933
Lucas Hardt <lucas.hardt@fu-berlin.de>
2024-05-06 15:45:48 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,176 @@
+'use client'
+
 import Image from "next/image";
+import React, {useState} from "react";
+import Link from "next/link";
+
+type OrganizationBoxProps = {
+    logoSrc: string;
+    orgLink: string;
+};
+
+const OrganizationBox: React.FC<OrganizationBoxProps> = ({logoSrc, orgLink}) => {
+    return (
+        <Link href={orgLink}>
+            <div className="bg-white rounded-lg shadow-md relative overflow-hidden cursor-pointer">
+                <div className="h-28 w-56 content-center">
+                    <Image
+                        src={logoSrc}
+                        alt="Organization Logo"
+                        width={500}
+                        height={128}
+                    />
+                </div>
+            </div>
+        </Link>
+    );
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [isOpen, setIsOpen] = useState(false);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    return (
+        <main className="flex flex-col min-h-screen bg-gray-100">
+            <nav className="bg-gray-800">
+                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div className="relative flex h-16 items-center justify-between">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            <button
+                                type="button"
+                                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                aria-controls="mobile-menu"
+                                aria-expanded="false"
+                                onClick={() => setIsOpen(!isOpen)}
+                            >
+                                <span className="sr-only">Open main menu</span>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                                {!isOpen && (
+                                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                         stroke="currentColor"
+                                         aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                                    </svg>
+                                )}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                                {isOpen && (
+                                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                         stroke="currentColor"
+                                         aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                                    </svg>
+                                )}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                            </button>
+                        </div>
+                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                            <div className="flex flex-shrink-0 items-center">
+                                <Image
+                                    src="/logo.png"
+                                    alt="eduMFA Logo"
+                                    width={64}
+                                    height={64}
+                                    priority
+                                />
+                            </div>
+                            <div className="hidden sm:ml-6 sm:block">
+                                <div className="flex space-x-4">
+                                    <Link href="https://github.com/eduMFA/eduMFA/"
+                                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Github</Link>
+                                    <Link href="https://edumfa.readthedocs.io/"
+                                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Documentation</Link>
+                                    <Link href="https://www.listserv.dfn.de/sympa/info/edumfa-users"
+                                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Mailing
+                                        List</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                {isOpen && (
+                    <div className="sm:hidden" id="mobile-menu">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            <Link href="https://github.com/eduMFA/eduMFA/"
+                                  className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Github</Link>
+                            <Link href="https://edumfa.readthedocs.io/"
+                                  className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Documentation</Link>
+                            <Link href="https://www.listserv.dfn.de/sympa/info/edumfa-users"
+                                  className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Mailing
+                                List</Link>
+                        </div>
+                    </div>
+                )}
+            </nav>
+
+            <div className="container mx-auto px-4 py-8 flex-grow">
+                <section className="mb-8">
+                    <h2 className="text-2xl font-bold mb-4">About eduMFA</h2>
+                    <p className="text-gray-700">
+                        eduMFA is a modular authentication system that enhances existing applications with a second
+                        factor
+                        during authentication, boosting security. Originally used for OTP authentication devices, it now
+                        supports other methods like challenge response and SSH keys. It runs on Linux and is completely
+                        open
+                        source, licensed under the AGPLv3.
+                    </p>
+                </section>
+
+                <section className="mb-8">
+                    <h2 className="text-2xl font-bold mb-4">Why eduMFA?</h2>
+                    <p className="text-gray-700">
+                        eduMFA provides enhanced protection against hacking attacks for IT services in the academic
+                        network. It
+                        enables multi-factor authentication, making traditional username-password combinations obsolete.
+                        The
+                        'Passkeys' method, a focus of eduMFA, locks a security key into a safe, accessible only through
+                        additional
+                        methods like biometric scans or PINs. Passkeys are resistant to phishing attacks, can be
+                        synchronized
+                        between compatible devices, and offer a seamless user experience with strong, secure
+                        authentication. eduMFA
+                        is already in use at several universities and is being further developed according to the
+                        requirements of
+                        the scientific community.
+                    </p>
+                </section>
+
+                <section className="mb-8">
+                    <h2 className="text-2xl font-bold mb-4">Organizations Using eduMFA</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                        <OrganizationBox logoSrc={'/organizations/fu_berlin.png'} orgLink={'https://fu-berlin.de'}/>
+                        <OrganizationBox logoSrc={'/organizations/hm.png'} orgLink={'https://hm.edu/'}/>
+                        <OrganizationBox logoSrc={'/organizations/uni_bamberg.png'}
+                                         orgLink={'https://www.uni-bamberg.de/'}/>
+                    </div>
+                </section>
+
+
+                <section className="mb-8">
+                    <h2 className="text-2xl font-bold mb-4">Installation</h2>
+                    <p className="text-gray-700">
+                        The system is written in Python, uses Flask as the web framework, and an SQL database as the
+                        datastore.
+                        Installation is straightforward, providing a lean installation process.
+                    </p>
+                    <br/>
+                    <p className="text-gray-700">
+                        You can learn more about the installation process in the <a
+                        href="https://edumfa.readthedocs.io/en/latest/installation/index.html"
+                        className="text-blue-600 hover:underline">documentation</a>.
+                    </p>
+                </section>
+
+            </div>
+
+            <footer className="bg-gray-800 text-white py-4">
+                <div className="container mx-auto px-4 text-center">
+                    <p>&copy; 2024 eduMFA. All rights reserved.</p>
+                    <a href="imprint.html" className="text-white hover:text-gray-300">Imprint</a>
+                </div>
+            </footer>
+        </main>
+    );
 }
