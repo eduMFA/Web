e6b663bea81ef9779c7fd1771219051afc709a4f
Lucas Hardt <lucas.hardt@fu-berlin.de>
2024-07-09 09:19:31 +0200

    update svg attributes

--- app/page.tsx
+++ app/page.tsx
@@ -49,8 +49,8 @@ const OrganizationBox: React.FC<OrganizationBoxProps> = ({user}) => {
                                 {user.userCount != undefined && (
                                     <div className="flex items-center mb-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             stroke-width="1.5" stroke="currentColor" className="size-5 mr-2">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                             strokeWidth="1.5" stroke="currentColor" className="size-5 mr-2">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
                                                   d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
                                         </svg>
                                         <p className=" text-gray-700">Approx. User Count: <span
@@ -63,8 +63,8 @@ const OrganizationBox: React.FC<OrganizationBoxProps> = ({user}) => {
                                 {user.phase && (
                                     <div className="flex items-center mb-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             stroke-width="1.5" stroke="currentColor" className="size-5 mr-2">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                             strokeWidth="1.5" stroke="currentColor" className="size-5 mr-2">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
                                                   d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"/>
                                         </svg>
                                         <p className=" text-gray-700 pr-2">Implementation Phase: </p>
@@ -89,8 +89,8 @@ const OrganizationBox: React.FC<OrganizationBoxProps> = ({user}) => {
                                     <div className="mb-4">
                                         <div className="flex items-center mb-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 stroke-width="1.5" stroke="currentColor" className="size-6 mr-2">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                 strokeWidth="1.5" stroke="currentColor" className="size-6 mr-2">
+                                                <path strokeLinecap="round" strokeLinejoin="round"
                                                       d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
                                             </svg>
                                             <p className="text-gray-700">Token Types:</p>
