75d32d393eaebd1a3597e5f820bc69ca6f381cd0
Lucas Hardt <lucas.hardt@fu-berlin.de>
2024-07-11 11:48:25 +0200

    Move organization box into seperated component

--- app/page.tsx
+++ app/page.tsx
@@ -12,107 +12,12 @@ import {
 import Image from "next/image";
 import React from "react";
 import organizationData from "@/data/users.json";
-import {OrganizationImplementationPhase, User} from "@/types/organizationTypes";
-import {Card} from "@nextui-org/card";
-import {Modal, ModalBody, ModalContent, ModalHeader, useDisclosure} from "@nextui-org/modal";
+import {User} from "@/types/organizationTypes";
 import {Link} from "@nextui-org/link";
-import {Tooltip} from "@nextui-org/tooltip";
-import {Chip} from "@nextui-org/chip";
 import {ScrollShadow} from "@nextui-org/scroll-shadow";
+import {OrganizationBox} from "@/components/organizationBox";
 
 const organizations = organizationData as User[];
-
-interface OrganizationBoxProps {
-    user: User
-}
-const OrganizationBox: React.FC<OrganizationBoxProps> = ({user}) => {
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-
-    return (
-        <>
-            <Card isPressable className="h-32 content-center border-none" onPress={() => onOpen()}>
-                <Image
-                    src={user.logoSrc}
-                    alt={user.name}
-                    fill={true}
-                    className={'p-2 object-contain'}
-                />
-            </Card>
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader  className="flex flex-col gap-1">
-                                <Link isExternal showAnchorIcon href={user.link} color="foreground">{user.name}</Link>
-                            </ModalHeader>
-                            <ModalBody>
-                                {user.userCount != undefined && (
-                                    <div className="flex items-center mb-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             strokeWidth="1.5" stroke="currentColor" className="size-5 mr-2">
-                                            <path strokeLinecap="round" strokeLinejoin="round"
-                                                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
-                                        </svg>
-                                        <p className=" text-gray-700">Approx. User Count: <span
-                                            className="font-bold">{new Intl.NumberFormat('en', {
-                                            notation: 'compact',
-                                            maximumFractionDigits: 2
-                                        },).format(user.userCount)}</span></p>
-                                    </div>
-                                )}
-                                {user.phase && (
-                                    <div className="flex items-center mb-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             strokeWidth="1.5" stroke="currentColor" className="size-5 mr-2">
-                                            <path strokeLinecap="round" strokeLinejoin="round"
-                                                  d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"/>
-                                        </svg>
-                                        <p className=" text-gray-700 pr-2">Implementation Phase: </p>
-                                        <Tooltip showArrow content="Evaluation">
-                                            <div className={`w-6 h-6 rounded-full flex items-center justify-center text-sm font-bold ${user.phase === OrganizationImplementationPhase.EVALUATION ? 'bg-blue-500 text-white' : 'bg-gray-300'} mr-2 group relative`}>
-                                                E
-                                            </div>
-                                        </Tooltip>
-                                        <Tooltip showArrow content="Testing">
-                                            <div className={`w-6 h-6 rounded-full flex items-center justify-center text-sm font-bold ${user.phase === OrganizationImplementationPhase.TESTING ? 'bg-yellow-500 text-black' : 'bg-gray-300'} mr-2 group relative`}>
-                                                T
-                                            </div>
-                                        </Tooltip>
-                                        <Tooltip showArrow content="Production">
-                                            <div className={`w-6 h-6 rounded-full flex items-center justify-center text-sm font-bold ${user.phase === OrganizationImplementationPhase.PRODUCTION ? 'bg-green-500 text-white' : 'bg-gray-300'} mr-2 group relative`}>
-                                                P
-                                            </div>
-                                        </Tooltip>
-                                    </div>
-                                )}
-                                {user.tokenTypes && user.tokenTypes.length > 0 && (
-                                    <div className="mb-4">
-                                        <div className="flex items-center mb-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 strokeWidth="1.5" stroke="currentColor" className="size-6 mr-2">
-                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
-                                            </svg>
-                                            <p className="text-gray-700">Token Types:</p>
-                                        </div>
-                                        <div className="flex gap-2 flex-wrap">
-                                            {user.tokenTypes.map((type, index) => (
-                                                <Chip key={index}>{type}</Chip>
-                                            ))}
-                                        </div>
-                                    </div>
-                                )}
-                                {user.userCount == undefined && user.phase == undefined && (user.tokenTypes == undefined || user.tokenTypes.length === 0) && (
-                                    <p className="text-gray-700 mb-4">No additional information available.</p>
-                                )}
-                            </ModalBody>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
-        </>
-    );
-}
 
 export default function Home() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
