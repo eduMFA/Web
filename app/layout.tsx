efeaaebe05294a12021708394aa20815858cafd1
Lucas Hardt <Luc1412.lh@gmail.com>
2025-02-03 12:55:16 +0100

    Add open graph meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,11 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata: Metadata = {
     title: "eduMFA",
     description: "Up-to-date multifactor authentication for academic institutions",
+    openGraph: {
+        title: "eduMFA",
+        type: "website",
+        url: "https://edumfa.com",
+    }
 };
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
